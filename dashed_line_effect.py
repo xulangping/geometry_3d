f59a64b10351e1d7b366acb4d150bf995b65c8c6
xulangping <xlp@mail.ustc.edu.cn>
2017-08-11 10:53:34 +0800

    修复bug
    测试通过

--- dashed_line_effect.py
+++ dashed_line_effect.py
@@ -13,7 +13,7 @@ def three_pyramid_volume(s):
     x2, y2, z2 = xiangliang(s[1], s[3])
     x3, y3, z3 = xiangliang(s[2], s[3])
     M = Matrix([[x1, y1, z1], [x2, y2, z2], [x3, y3, z3]])
-    v = abs(det(M)) / simplify(6)
+    v = det(M) / simplify(6)
     return v
 def point_in_plane(s, P):
     if area([s[0], s[1], P]) + area([s[0], s[2], P]) + area([s[2], s[1], P]) == area(s):
@@ -27,6 +27,7 @@ def if_line_intersection(s1, s2):
         P.append(x * (s2[1][i] - s2[0][i]) + s2[0][i])
     s1.append(P)
     t = solve(three_pyramid_volume(s1))
+    t = t[0]
     for i in range(3):
         Q.append(t * (s2[1][i] - s2[0][i]) + s2[0][i])
     if (t >= 1) or (t <= 0) or (not point_in_plane(s1, Q)):
@@ -39,8 +40,18 @@ def find_dotted_line(s1, s2, P):
         for j in range(3):
             b.append((i[0][j] - i[1][j]) / simplify(2))
         for j in list(itertools.combinations(s2,3)):
+            j = list(j)
             if area(j) > 0:
                 if if_line_intersection(j, [P, b]):
                     a.append(i)
                     break
     return a
+A = [0, 0, 0]
+B = [1, 0, 0]
+C = [0, 1, 0]
+D = [0, 0, 1]
+E = [0, 1, 1]
+F = [1, 0, 1]
+G = [1, 1, 0]
+H = [1, 1, 1]
+print(find_dotted_line([[A, B], [A, C], [A, D], [G, H]], [A, B, C, D], [2, 2, 2]))
